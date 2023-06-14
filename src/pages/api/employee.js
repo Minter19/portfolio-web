53a954c98e869fe7937e561233afc378ed88553d
Minter Prasetyo <mprgukguk@patranusa.com>
2023-06-14 14:13:58 +0700

    add rest static employee

---
+++ src/pages/api/employee.js
@@ -0,0 +1,730 @@
+
+let data = [
+    {
+      "id": 1,
+      "nama": "John Doe",
+      "alamat": "Jl. Merdeka No. 123",
+      "tgl_lahir": "1975-09-12",
+      "jabatan": "Manager",
+      "umur": 48,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 2,
+      "nama": "Jane Smith",
+      "alamat": "Jl. Raya Sudirman No. 45",
+      "tgl_lahir": "1982-03-25",
+      "jabatan": "Developer",
+      "umur": 41,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 3,
+      "nama": "Michael Johnson",
+      "alamat": "Jl. Pahlawan No. 67",
+      "tgl_lahir": "1978-11-08",
+      "jabatan": "Analyst",
+      "umur": 45,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 4,
+      "nama": "Emily Davis",
+      "alamat": "Jl. Gajah Mada No. 21",
+      "tgl_lahir": "1990-07-15",
+      "jabatan": "Designer",
+      "umur": 33,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 5,
+      "nama": "Christopher Wilson",
+      "alamat": "Jl. Diponegoro No. 88",
+      "tgl_lahir": "1965-02-19",
+      "jabatan": "Project Manager",
+      "umur": 58,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 6,
+      "nama": "Olivia Brown",
+      "alamat": "Jl. Ahmad Yani No. 5",
+      "tgl_lahir": "1986-06-30",
+      "jabatan": "Tester",
+      "umur": 37,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 7,
+      "nama": "William Taylor",
+      "alamat": "Jl. Veteran No. 15",
+      "tgl_lahir": "1973-04-03",
+      "jabatan": "Developer",
+      "umur": 50,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 8,
+      "nama": "Sophia Anderson",
+      "alamat": "Jl. Pemuda No. 72",
+      "tgl_lahir": "1988-09-21",
+      "jabatan": "Designer",
+      "umur": 35,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 9,
+      "nama": "Alexander Martinez",
+      "alamat": "Jl. Siliwangi No. 17",
+      "tgl_lahir": "1977-11-29",
+      "jabatan": "Analyst",
+      "umur": 46,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 10,
+      "nama": "Isabella Thomas",
+      "alamat": "Jl. Cendana No. 34",
+      "tgl_lahir": "1995-08-14",
+      "jabatan": "Manager",
+      "umur": 28,
+      "jenis_kelamin": "Perempuan",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 11,
+      "nama": "Ethan Hernandez",
+      "alamat": "Jl. Puri Indah No. 9",
+      "tgl_lahir": "1981-02-02",
+      "jabatan": "Developer",
+      "umur": 42,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 12,
+      "nama": "Mia Walker",
+      "alamat": "Jl. Melati No. 56",
+      "tgl_lahir": "1989-07-26",
+      "jabatan": "Tester",
+      "umur": 34,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 13,
+      "nama": "Aiden Lopez",
+      "alamat": "Jl. Mangga No. 87",
+      "tgl_lahir": "1972-05-07",
+      "jabatan": "Designer",
+      "umur": 51,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 14,
+      "nama": "Charlotte Perez",
+      "alamat": "Jl. Merpati No. 22",
+      "tgl_lahir": "1987-12-18",
+      "jabatan": "Analyst",
+      "umur": 36,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 15,
+      "nama": "James Moore",
+      "alamat": "Jl. Surya Kencana No. 39",
+      "tgl_lahir": "1993-09-03",
+      "jabatan": "Project Manager",
+      "umur": 30,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 16,
+      "nama": "Amelia Sanchez",
+      "alamat": "Jl. Diponegoro No. 76",
+      "tgl_lahir": "1979-01-14",
+      "jabatan": "Developer",
+      "umur": 44,
+      "jenis_kelamin": "Perempuan",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 17,
+      "nama": "Benjamin Torres",
+      "alamat": "Jl. Raya Bogor No. 50",
+      "tgl_lahir": "1998-06-10",
+      "jabatan": "Designer",
+      "umur": 25,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 18,
+      "nama": "Liam Rivera",
+      "alamat": "Jl. Ganesha No. 11",
+      "tgl_lahir": "1971-03-27",
+      "jabatan": "Analyst",
+      "umur": 52,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 19,
+      "nama": "Sofia Gómez",
+      "alamat": "Jl. Gatot Subroto No. 30",
+      "tgl_lahir": "1992-11-23",
+      "jabatan": "Manager",
+      "umur": 31,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 20,
+      "nama": "Noah Lee",
+      "alamat": "Jl. Pahlawan No. 14",
+      "tgl_lahir": "1984-04-29",
+      "jabatan": "Tester",
+      "umur": 39,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 21,
+      "nama": "Lily Ward",
+      "alamat": "Jl. Thamrin No. 62",
+      "tgl_lahir": "1994-08-17",
+      "jabatan": "Developer",
+      "umur": 29,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 22,
+      "nama": "Lucas García",
+      "alamat": "Jl. Veteran No. 85",
+      "tgl_lahir": "1976-12-09",
+      "jabatan": "Analyst",
+      "umur": 47,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 23,
+      "nama": "Ava Young",
+      "alamat": "Jl. Sudirman No. 33",
+      "tgl_lahir": "1983-02-05",
+      "jabatan": "Designer",
+      "umur": 40,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 24,
+      "nama": "Carter Scott",
+      "alamat": "Jl. Raya Merdeka No. 51",
+      "tgl_lahir": "1974-07-22",
+      "jabatan": "Project Manager",
+      "umur": 49,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 25,
+      "nama": "Mila Clark",
+      "alamat": "Jl. Imam Bonjol No. 48",
+      "tgl_lahir": "1991-05-11",
+      "jabatan": "Developer",
+      "umur": 32,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 26,
+      "nama": "Henry Adams",
+      "alamat": "Jl. Asia Afrika No. 19",
+      "tgl_lahir": "1970-09-07",
+      "jabatan": "Tester",
+      "umur": 53,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 27,
+      "nama": "Ella Lewis",
+      "alamat": "Jl. Pemuda No. 69",
+      "tgl_lahir": "1980-06-28",
+      "jabatan": "Designer",
+      "umur": 43,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 28,
+      "nama": "Sebastian Mitchell",
+      "alamat": "Jl. Diponegoro No. 23",
+      "tgl_lahir": "1997-12-15",
+      "jabatan": "Analyst",
+      "umur": 26,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 29,
+      "nama": "Scarlett King",
+      "alamat": "Jl. Surya Kencana No. 57",
+      "tgl_lahir": "1985-03-09",
+      "jabatan": "Manager",
+      "umur": 38,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 30,
+      "nama": "David Wood",
+      "alamat": "Jl. Melati No. 16",
+      "tgl_lahir": "1973-11-01",
+      "jabatan": "Developer",
+      "umur": 50,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 31,
+      "nama": "Victoria Baker",
+      "alamat": "Jl. Mangga No. 66",
+      "tgl_lahir": "1996-08-26",
+      "jabatan": "Tester",
+      "umur": 27,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 32,
+      "nama": "Joseph Green",
+      "alamat": "Jl. Gatot Subroto No. 28",
+      "tgl_lahir": "1977-05-05",
+      "jabatan": "Designer",
+      "umur": 46,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 33,
+      "nama": "Penelope Price",
+      "alamat": "Jl. Veteran No. 10",
+      "tgl_lahir": "1984-12-20",
+      "jabatan": "Analyst",
+      "umur": 39,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 34,
+      "nama": "Daniel Cooper",
+      "alamat": "Jl. Sudirman No. 41",
+      "tgl_lahir": "1990-09-10",
+      "jabatan": "Project Manager",
+      "umur": 33,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 35,
+      "nama": "Natalie Ross",
+      "alamat": "Jl. Raya Bogor No. 55",
+      "tgl_lahir": "1971-06-16",
+      "jabatan": "Developer",
+      "umur": 52,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 36,
+      "nama": "Christopher Ward",
+      "alamat": "Jl. Ganesha No. 13",
+      "tgl_lahir": "1979-09-03",
+      "jabatan": "Tester",
+      "umur": 44,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 37,
+      "nama": "Hannah Foster",
+      "alamat": "Jl. Thamrin No. 68",
+      "tgl_lahir": "1986-07-12",
+      "jabatan": "Designer",
+      "umur": 37,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 38,
+      "nama": "Matthew Hill",
+      "alamat": "Jl. Raya Merdeka No. 43",
+      "tgl_lahir": "1975-04-25",
+      "jabatan": "Analyst",
+      "umur": 48,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 39,
+      "nama": "Addison Phillips",
+      "alamat": "Jl. Imam Bonjol No. 33",
+      "tgl_lahir": "1993-11-19",
+      "jabatan": "Manager",
+      "umur": 30,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 40,
+      "nama": "Jackson Allen",
+      "alamat": "Jl. Asia Afrika No. 15",
+      "tgl_lahir": "1981-02-14",
+      "jabatan": "Developer",
+      "umur": 42,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 41,
+      "nama": "Grace Howard",
+      "alamat": "Jl. Pemuda No. 63",
+      "tgl_lahir": "1988-09-21",
+      "jabatan": "Tester",
+      "umur": 35,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 42,
+      "nama": "Jack Campbell",
+      "alamat": "Jl. Diponegoro No. 41",
+      "tgl_lahir": "1972-05-07",
+      "jabatan": "Designer",
+      "umur": 51,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 43,
+      "nama": "Avery Roberts",
+      "alamat": "Jl. Merpati No. 14",
+      "tgl_lahir": "1987-12-18",
+      "jabatan": "Analyst",
+      "umur": 36,
+      "jenis_kelamin": "Perempuan",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 44,
+      "nama": "Evelyn Ross",
+      "alamat": "Jl. Surya Kencana No. 29",
+      "tgl_lahir": "1993-09-03",
+      "jabatan": "Project Manager",
+      "umur": 30,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 45,
+      "nama": "Daniel Powell",
+      "alamat": "Jl. Diponegoro No. 76",
+      "tgl_lahir": "1979-01-14",
+      "jabatan": "Developer",
+      "umur": 44,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 46,
+      "nama": "Olivia Simmons",
+      "alamat": "Jl. Raya Bogor No. 50",
+      "tgl_lahir": "1998-06-10",
+      "jabatan": "Designer",
+      "umur": 25,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 47,
+      "nama": "Logan Hughes",
+      "alamat": "Jl. Ganesha No. 11",
+      "tgl_lahir": "1971-03-27",
+      "jabatan": "Analyst",
+      "umur": 52,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 48,
+      "nama": "Emma Cook",
+      "alamat": "Jl. Gatot Subroto No. 30",
+      "tgl_lahir": "1992-11-23",
+      "jabatan": "Manager",
+      "umur": 31,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 49,
+      "nama": "Michael Butler",
+      "alamat": "Jl. Pahlawan No. 14",
+      "tgl_lahir": "1984-04-29",
+      "jabatan": "Tester",
+      "umur": 39,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 50,
+      "nama": "Emily Adams",
+      "alamat": "Jl. Thamrin No. 62",
+      "tgl_lahir": "1994-08-17",
+      "jabatan": "Developer",
+      "umur": 29,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 51,
+      "nama": "Matthew Martin",
+      "alamat": "Jl. Veteran No. 85",
+      "tgl_lahir": "1976-12-09",
+      "jabatan": "Analyst",
+      "umur": 47,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 52,
+      "nama": "Aria Hill",
+      "alamat": "Jl. Sudirman No. 33",
+      "tgl_lahir": "1983-02-05",
+      "jabatan": "Designer",
+      "umur": 40,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 53,
+      "nama": "Alexander Peterson",
+      "alamat": "Jl. Raya Merdeka No. 51",
+      "tgl_lahir": "1974-07-22",
+      "jabatan": "Project Manager",
+      "umur": 49,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 54,
+      "nama": "Scarlett Flores",
+      "alamat": "Jl. Imam Bonjol No. 48",
+      "tgl_lahir": "1991-05-11",
+      "jabatan": "Developer",
+      "umur": 32,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 55,
+      "nama": "Liam Evans",
+      "alamat": "Jl. Asia Afrika No. 19",
+      "tgl_lahir": "1970-09-07",
+      "jabatan": "Tester",
+      "umur": 53,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 56,
+      "nama": "Chloe Mitchell",
+      "alamat": "Jl. Pemuda No. 69",
+      "tgl_lahir": "1980-06-28",
+      "jabatan": "Designer",
+      "umur": 43,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 57,
+      "nama": "Elijah Turner",
+      "alamat": "Jl. Diponegoro No. 23",
+      "tgl_lahir": "1997-12-15",
+      "jabatan": "Analyst",
+      "umur": 26,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 58,
+      "nama": "Sofia Stewart",
+      "alamat": "Jl. Surya Kencana No. 57",
+      "tgl_lahir": "1985-03-09",
+      "jabatan": "Manager",
+      "umur": 38,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 59,
+      "nama": "Owen Hill",
+      "alamat": "Jl. Melati No. 16",
+      "tgl_lahir": "1973-11-01",
+      "jabatan": "Developer",
+      "umur": 50,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 60,
+      "nama": "Eva Martinez",
+      "alamat": "Jl. Mangga No. 66",
+      "tgl_lahir": "1996-08-26",
+      "jabatan": "Tester",
+      "umur": 27,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 61,
+      "nama": "James Murphy",
+      "alamat": "Jl. Gatot Subroto No. 28",
+      "tgl_lahir": "1977-05-05",
+      "jabatan": "Designer",
+      "umur": 46,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 62,
+      "nama": "Luna Phillips",
+      "alamat": "Jl. Veteran No. 10",
+      "tgl_lahir": "1984-12-20",
+      "jabatan": "Analyst",
+      "umur": 39,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 63,
+      "nama": "Jacob Turner",
+      "alamat": "Jl. Sudirman No. 41",
+      "tgl_lahir": "1990-09-10",
+      "jabatan": "Project Manager",
+      "umur": 33,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 64,
+      "nama": "Aurora Green",
+      "alamat": "Jl. Raya Bogor No. 55",
+      "tgl_lahir": "1971-06-16",
+      "jabatan": "Developer",
+      "umur": 52,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 65,
+      "nama": "Ethan Ward",
+      "alamat": "Jl. Ganesha No. 13",
+      "tgl_lahir": "1979-09-03",
+      "jabatan": "Tester",
+      "umur": 44,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 66,
+      "nama": "Bella Foster",
+      "alamat": "Jl. Thamrin No. 68",
+      "tgl_lahir": "1986-07-12",
+      "jabatan": "Designer",
+      "umur": 37,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 67,
+      "nama": "Noah Hill",
+      "alamat": "Jl. Raya Merdeka No. 43",
+      "tgl_lahir": "1975-04-25",
+      "jabatan": "Analyst",
+      "umur": 48,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 68,
+      "nama": "Mila Phillips",
+      "alamat": "Jl. Imam Bonjol No. 33",
+      "tgl_lahir": "1993-11-19",
+      "jabatan": "Manager",
+      "umur": 30,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    },
+    {
+      "id": 69,
+      "nama": "Carter Allen",
+      "alamat": "Jl. Asia Afrika No. 15",
+      "tgl_lahir": "1981-02-14",
+      "jabatan": "Developer",
+      "umur": 42,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 70,
+      "nama": "Ava Howard",
+      "alamat": "Jl. Pemuda No. 63",
+      "tgl_lahir": "1988-09-21",
+      "jabatan": "Tester",
+      "umur": 35,
+      "jenis_kelamin": "Perempuan",
+      "status": "Non Aktif"
+    },
+    {
+      "id": 71,
+      "nama": "Mason Campbell",
+      "alamat": "Jl. Diponegoro No. 41",
+      "tgl_lahir": "1972-05-07",
+      "jabatan": "Designer",
+      "umur": 51,
+      "jenis_kelamin": "Laki-laki",
+      "status": "Aktif"
+    },
+    {
+      "id": 72,
+      "nama": "Aria Roberts",
+      "alamat": "Jl. Merpati No. 14",
+      "tgl_lahir": "1987-12-18",
+      "jabatan": "Analyst",
+      "umur": 36,
+      "jenis_kelamin": "Perempuan",
+      "status": "Aktif"
+    }
+]  
+
+
+export default function employee(req, res) {
+    res.status(200).json(
+        data
+    )
+}
